e31d206c5291cfd63c037a37c4974764a9e71fd2
Ammighorbani <Ammighorbani1385@gmail.com>
2023-12-03 02:19:28 +0330

    My.Codes

--- #1 Code.py
+++ #1 Code.py
@@ -1,1 +1,13 @@
-#1 Code
+x = int(input())
+y = int(input())
+
+print(x+y) 
+# to add x to y it could be  chage to  y + x
+print(x-y)
+# to mines y of x it could be change to y - x
+print(x*y)
+# to multiply x to y it could be change to y * x
+print(x/y)
+# to divide x to y it could be change to y / x
+print(x**y)
+# to power x to y it could be change to y ** x
